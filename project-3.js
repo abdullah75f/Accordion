b0b254db6814ee649dfb36531b917ef0df26ec48
zaz7 <abdullah75farid@gmail.com>
2024-07-12 22:18:01 +0300

    xxx

--- project-3.js
+++ project-3.js
@@ -4,7 +4,7 @@ const accordion = document.getElementsByClassName("content-container");
 
 for( i = 0; i < accordion.length; i++){
     accordion[i].addEventListener("click", function(){
-        this
+        this.classList.toggle("active");
     })
 
 }
